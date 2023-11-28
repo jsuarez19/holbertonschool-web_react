3838dba79edb134fc6e682752f216d2ce52d3978
Jose Suarez <jolusuvi@gmail.com>
2023-11-28 00:30:22 -0500

    Embedding expressions, functions

---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,12 @@
+//return the current year
+export const getFullYear = () => {
+  return new Date().getFullYear();
+};
+
+export const getFooterCopy = (isIndex) => {
+  let res = undefined;
+  isIndex
+    ? (res = 'Holberton School')
+    : (res = 'Holberton School main dashboard');
+  return res;
+};
