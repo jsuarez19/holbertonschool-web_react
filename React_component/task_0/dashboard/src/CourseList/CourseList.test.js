720aa9da2a684bdf912822aee1dab7783496ad60
Jose Suarez <jolusuvi@gmail.com>
2023-11-30 18:08:58 -0500

    Test updated

--- React_component/task_0/dashboard/src/CourseList/CourseList.test.js
+++ React_component/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -8,6 +8,16 @@ describe('<CourseList />', () => {
   it('renders without crashing', () => {
     expect(wrapper.exists()).toBe(true);
   });
+
+  it('renders rows', () => {
+    const row = wrapper.find('CourseListRow');
+    expect(row.at(0).prop('textFirstCell')).toEqual('Available courses');
+    expect(row.at(0).prop('isHeader')).toEqual(true);
+    expect(row.at(1).prop('textFirstCell')).toEqual('Course name');
+    expect(row.at(1).prop('textSecondCell')).toEqual('Credit');
+    expect(row.at(1).prop('isHeader')).toEqual(true);
+    expect(row.at(2).prop('isHeader')).toEqual(false);
+  });
 });
 
 describe('listCourses without values', () => {
