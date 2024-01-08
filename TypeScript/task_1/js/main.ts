dc249ad61e6ed411778c4ab7ba7d3aa32014b236
Jose Suarez <jolusuvi@gmail.com>
2024-01-07 20:42:44 -0500

    Creating printTeacher interface and function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -10,3 +10,11 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string) {
+  return(`${firstName.charAt(0)}. ${lastName}`);
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
