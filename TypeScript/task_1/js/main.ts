7fa7e7a5f69fe20a1d975663897ba27af98268f8
Jose Suarez <jolusuvi@gmail.com>
2024-01-07 20:18:39 -0500

    Creating teacher interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,29 +1,8 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
   location: string;
+  [key:string]: any;
 }
-
-declare const student1: Student
-declare const student2: Student
-const studentsList: Student[] = [student1, student2];
-
-document.addEventListener("DOMContentLoaded", () => {
-  const tableBody = document.getElementById("tableBody");
-  
-  if (tableBody) {
-      studentsList.forEach(student => {
-          const row = document.createElement("tr");
-          const firstNameCell = document.createElement("td");
-          const locationCell = document.createElement("td");
-          
-          firstNameCell.textContent = student.firstName;
-          locationCell.textContent = student.location;
-          
-          row.appendChild(firstNameCell);
-          row.appendChild(locationCell);
-          tableBody.appendChild(row);
-      });
-  }
-})
