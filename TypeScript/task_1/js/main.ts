ea3587f07cefbc7ae4c211033e16ed18172162ec
Jose Suarez <jolusuvi@gmail.com>
2024-01-07 21:02:49 -0500

    Writing a Class named StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,10 +11,39 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+// Function that prints teacher
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 function printTeacher(firstName: string, lastName: string) {
   return(`${firstName.charAt(0)}. ${lastName}`);
 }
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+// Class that contains student
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
