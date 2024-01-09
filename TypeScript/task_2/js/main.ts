28372189e25207f89e99d2f763e29de2f72ee8a7
Jose Suarez <jolusuvi@gmail.com>
2024-01-09 10:41:29 -0500

    Creating the classes Director and Teacher

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,41 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome() {
+    return 'Working from home';
+  }
+  getCoffeeBreak() {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks() {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome() {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak() {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks() {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: string | number): Teacher | Director {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else { return new Director(); }
+}
