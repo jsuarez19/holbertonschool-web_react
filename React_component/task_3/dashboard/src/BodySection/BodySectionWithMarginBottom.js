83bc9e5a8f12ef0eb2443a9500cf7744e88989ea
Jose Suarez <jolusuvi@gmail.com>
2024-01-23 02:06:49 -0500

    Specialization

---
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,23 @@
+import React from "react";
+import BodySection from "./BodySection";
+import './BodySectionWithMarginBottom.css';
+
+class BodySectionWithMarginBottom extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="bodySectionWithMargin">
+        <BodySection {...this.props} />
+      </div>
+    )
+  }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+  ...BodySection.propTypes,
+};
+
+export default BodySectionWithMarginBottom;
