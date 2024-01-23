e4f5be8c4082f3a793591ce7bd65caea257906cf
Jose Suarez <jolusuvi@gmail.com>
2024-01-23 02:16:24 -0500

    Use the new components

---
+++ React_component/task_3/dashboard/src/App/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Notifications from '../Notifications/Notifications'
+import Footer from '../Footer/Footer'
+import Login from '../Login/Login'
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+const listNotifications = [
+  { id:1, type: 'default', value: 'New course available' },
+  { id:2, type: 'urgent', value: 'New resume available' },
+  { id:3, type: 'urgent', html: { __html: getLatestNotification() } },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: props.isLoggedIn,
+    };
+  }
+
+  componentDidMount() {
+    // Add event listener when component is mounted
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    // Remove event listener when component is unmounted
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.state;
+    
+    let screen = undefined;
+    isLoggedIn 
+      ? (screen = <BodySectionWithMarginBottom title='Course list'> <CourseList listCourses={listCourses} /> </BodySectionWithMarginBottom>) 
+      : (screen = <BodySectionWithMarginBottom title='Log in to continue'> <Login /> </BodySectionWithMarginBottom>);
+
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+        </div>
+        <div className='App-body'>
+          {screen}
+          <BodySection title='News from the School'>
+            <p>Holiday is coming!</p>
+          </BodySection>
+        </div>
+        <div className='App-footer'>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+export default App;
